c02cd5a122a8b09606c94a8d1cb734113e755dfe
MonotonousEcstasyOfDeath <78876776+MonotonousEcstasyOfDeath@users.noreply.github.com>
2022-05-05 22:12:32 +0300

    Add files via upload

---
+++ sketch/inst with node/uniqueValues.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const readline = require('readline');
+
+
+async function uniqueValues() {
+  let uniqSet = new Set();
+  for (let i = 0; i < 20; i++) {
+  let UsersFile = './instNicknames/out' + +i +'.txt';
+  const fileStream = fs.createReadStream(UsersFile);
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+  for await (const line of rl) {
+    uniqSet.add(line);
+  }
+  }
+  return console.log(uniqSet.size)
+}
+
+uniqueValues();
