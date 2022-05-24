6444907f72dbc43a644b210b8bb75794116d69d1
MonotonousEcstasyOfDeath <78876776+MonotonousEcstasyOfDeath@users.noreply.github.com>
2022-05-24 15:33:52 +0300

    hello async

--- геопозицияПоIP/expressapp.js
+++ геопозицияПоIP/expressapp.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const searchIpRange = require("./searchIpRange.js");
 
- 
+
 const app = express();
 app.use(function(request, response, next) {
      
     let ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress;
-    let ipInfo = searchIpRange(ip);
-    response.send(`<!DOCTYPE html>
-    <html>
-    <head>
-        <title>ip info</title>
-        <meta charset="utf-8" />
-    </head>
-    <body>
-        <h4>Range: ${ipInfo[0]} - ${ipInfo[1]}</h4>
-        <h3>${ipInfo[2]}: ${ipInfo[3]}</h3>
-    </body>
-    <html>`)
-});
+    // let ipInfo = searchIpRange(ip);
+    searchIpRange(ip).then(result => {
+        return response.send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>ip info</title>
+          <meta charset="utf-8" />
+        </head>
+        <body>
+          <h2>Range: ${Number(result[0].slice(1,-1))} - ${Number(result[1].slice(1,-1))}</h2>
+          <h2>${result[2].slice(1,-1)}: ${result[3].slice(1,-2)}</h2>
+        </body>
+        <html>`) 
+      });
+})
 
 
 app.listen(8080);
--- геопозицияПоIP/searchIpRange.js
+++ геопозицияПоIP/searchIpRange.js
@@ -1,57 +1,43 @@
+const fs = require('fs/promises');
+
 function IpConvert(ip) {
-    ip = ip.split('.');
-    ip = (+ip[0] * 16777216 + +ip[1] * 65536 + +ip[2] * 256 + +ip[3])
-    return ip;
+  ip = ip.split('.');
+  ip = (+ip[0] * 16777216 + +ip[1] * 65536 + +ip[2] * 256 + +ip[3])
+
+  return ip;
 }
 
 
-function searchDBpart(ip) {
-    const arrayDBparts = require("./DB/arrayDBparts.js");
-
-    let start = 0;
-    let end = arrayDBparts.length
-    let middle;
-    let found = false;
-
-    while (found === false && start <= end) {
-        middle = Math.floor((start + end) / 2);
-        if ((arrayDBparts[middle][1] <= ip) && (arrayDBparts[middle][2] >= ip)) {
-                found = true;
-                return ("./DB/" + arrayDBparts[middle][0]);
-                }
-        else if (ip < arrayDBparts[middle][1]) {
-            end = middle - 1;
-        } else {
-            start = middle + 1;
-        }
-    }
+async function importDB() {
+  try {
+    const data = await fs.readFile('./IP2LOCATION-LITE-DB1.CSV', { encoding: 'utf8' });
+    return data.split('\n');
+  } catch (err) {
+    console.log(err);
+  }
 }
+  
 
-
-function searchIpRange(ip) {
-    let res;
+async function searchIpRange(ip) {
     ip = IpConvert(ip);
-
-    const DBipRange = require(searchDBpart(ip));
+    const data = await importDB().then(result => {return result});
 
     let start = 0;
-    let end = DBipRange.length
+    let end = data.length
     let middle;
     let found = false;
-
     while (found === false && start <= end) {
-        middle = Math.floor((start + end) / 2);
-        if ((DBipRange[middle][0] <= ip) && (DBipRange[middle][1] >= ip)) {
-                found = true;
-                res = DBipRange[middle];
-                }
-        else if (ip < DBipRange[middle][0]) {
-            end = middle - 1;
-        } else {
-            start = middle + 1;
+      middle = Math.floor((start + end) / 2);
+    if (((Number((data[middle].split(','))[0].slice(1,-1))) <= ip) && ((Number((data[middle].split(','))[1].slice(1,-1))) >= ip)) {
+            found = true;
+            return data[middle].split(',');
         }
+    else if (ip < (Number(((data[middle].split(','))[0].slice(1,-1))))) {
+        end = middle - 1;
+    } else {
+        start = middle + 1;
     }
-    return res;
+  }
 }
 
 
