211ea98da6a8e220ac72ca6ff77596e34c8e587a
MonotonousEcstasyOfDeath <78876776+MonotonousEcstasyOfDeath@users.noreply.github.com>
2022-05-27 14:01:17 +0300

    Update FibonacciNumbers.js

--- algorithms and syntax/FibonacciNumbers.js
+++ algorithms and syntax/FibonacciNumbers.js
@@ -1,4 +1,3 @@
-// переименуй
 function fib(n) {
     let FibonacciNumbers = [1, 1];
    for (let i = 2; i <= n; i++) {
@@ -6,10 +5,3 @@ function fib(n) {
     }
     return FibonacciNumbers.at(-1);
 }
-
-console.log(fib(0));
-console.log(fib(1));
-console.log(fib(2));
-console.log(fib(3));
-console.log(fib(4));
-
