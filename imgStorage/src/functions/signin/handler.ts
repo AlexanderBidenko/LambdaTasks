f8d60744942b6f05170f073556be411a03abdc6a
MonotonousEcstasyOfDeath <Alex.bidenko15@gmail.com>
2022-11-04 15:52:01 +0200

    updated

--- imgStorage/src/functions/signin/handler.ts
+++ imgStorage/src/functions/signin/handler.ts
@@ -3,19 +3,18 @@ import { middyfy } from '@libs/lambda';
 import type { APIGatewayProxyResult} from '@libs/api-gateway';
 
 
-const signin =  async (): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: 
-    
-    `
-    <h2>You are sign in now.</h2>
-    <p><a href="https://img-storage-auth.auth.eu-west-2.amazoncognito.com/logout?response_type=code&client_id=2qeonfv87prllqa9cn5djrncuj&redirect_uri=https://xs8k89j49b.execute-api.us-east-1.amazonaws.com/dev/signin&state=STATE&scope=openid">Sign out</a></p>
-    `
-    
-    ,
-
-    headers: {['Content-type']: 'text/html'}
+const signin =  async (event): Promise<APIGatewayProxyResult> => {
+  if ((event.queryStringParameters != null) && (event.queryStringParameters != undefined) && (event.queryStringParameters != 'null') && (event.queryStringParameters != 'undefined')) {
+    return {
+      statusCode: 200,
+      body: `Hello world`
+      
+      ,
+  
+      headers: {
+        ["Content-Type"]: "application/json"
+    }
+  }
 
   }
 };
